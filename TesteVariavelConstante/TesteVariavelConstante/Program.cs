90af05dc1f2e19ca32894da511183bda4cc6afc2
Ana Cristina oliveira <121919367+GilAlvesOliveira@users.noreply.github.com>
2023-01-16 15:18:14 -0300

    Primeiro comit c#

---
+++ TesteVariavelConstante/TesteVariavelConstante/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TesteVariavelConstante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string constante = "uma constatnte que não muda";
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Favor informar um número inteiro");
+                return;
+            }
+            int inteiro;
+            bool teste = int.TryParse(args[0], out inteiro);
+
+            if (teste == false)
+            {
+                Console.WriteLine("O argumento informado nãi é um número inteiro");
+                return;
+            }
+
+            Console.WriteLine(constante);
+            Console.WriteLine("O número informado foi: " + inteiro);
+
+        }
+    }
+}
