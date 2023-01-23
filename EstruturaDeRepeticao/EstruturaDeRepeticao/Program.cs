6c4cb50dfa81d847b96f8f66485ef6e0fcafd561
Ana Cristina oliveira <121919367+GilAlvesOliveira@users.noreply.github.com>
2023-01-23 15:46:20 -0300

    estrutura de repetição

---
+++ EstruturaDeRepeticao/EstruturaDeRepeticao/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace EstruturaDeRepeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0) 
+            {
+                Console.WriteLine("Favor informar palavras no argumento");
+                return;
+            }
+            //for
+            for(var indice = 0; indice < args.Length; indice++) 
+            {
+            Console.WriteLine($"Estrutura for, indice e valor: {indice}, {args[indice]}");
+            }
+            //while
+            var argsLidos = 0;
+            while(argsLidos < args.Length) 
+            {
+                Console.WriteLine($"Estrutura while, argsLidos e valor lido: {argsLidos}, {args[argsLidos]}");
+                argsLidos++;
+            }
+            //do while
+            var loopsEfetuados = 0;
+            do
+            {
+                Console.WriteLine($"Estrutura do while, loopsEfetuado:{loopsEfetuados}, valor lido:{args[loopsEfetuados]}");
+                loopsEfetuados++;
+            }
+            while (loopsEfetuados < args.Length);
+           
+            //foreach
+
+        }
+    }
+}
